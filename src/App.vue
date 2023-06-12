9baaa5552e12eb6fb18c90d6e4e9e4357d97b1b5
InkCherryA <anniehe1126@gmail.com>
2023-06-12 00:31:36 -0400

    build test

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <h1 class="header">{{ selectedScene ? selectedScene + ' Demo' : 'Choose the demo' }}</h1>
 
     <div class="body">
-
       <div class="scene-list">
         <label v-for="(scene, index) in scenes" :key="index">
           <input type="radio" :value="scene" :checked="selectedScene === scene" @change="selectScene(scene)">
@@ -12,37 +11,35 @@
       </div>
 
       <div class="demo">
-        <component :is="selectedSceneComponent" v-if="selectedScene" />
+        <component :is="getSelectedSceneComponent" v-if="selectedScene" />
       </div>
-
     </div>
   </div>
 </template>
 
-
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import BlogEditor from './components/BlogEditor.vue';
-import Physics from './components/Physics.vue';
+  import { computed, ref } from 'vue';
+  import BlogEditor from './components/BlogEditor.vue';
+  import Physics from './components/Physics.vue';
 
-const scenes = ['Blog', 'Physics']; // Add other scene names here
+  const scenes = ['Blog', 'Physics']; // Add other scene names here
 
-const selectedScene = ref('');
+  const selectedScene = ref('');
 
-const selectedSceneComponent = computed(() => {
-  if (selectedScene.value === 'Blog') {
-    return BlogEditor;
-  } else if (selectedScene.value === 'Physics') {
-    return Physics;
+  const getSelectedSceneComponent = computed<any>(() => {
+    if (selectedScene.value === 'Blog') {
+      return BlogEditor;
+    } else if (selectedScene.value === 'Physics') {
+      return Physics;
+    }
+    // Add other scene components here
+  });
+
+  function selectScene(scene: string) {
+    selectedScene.value = scene;
   }
-  // Add other scene components here
-});
-
-function selectScene(scene: string) {
-  selectedScene.value = scene;
-}
-
 </script>
+
 
 <style scoped lang="scss">
   .header {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import './assets/main.scss'
 
 import { createApp } from 'vue'
+import BlogEditor from './components/BlogEditor.vue';
+import Physics from './components/Physics.vue';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component('blog-editor', BlogEditor);
+app.component('physics', Physics);
+
+app.mount('#app');
