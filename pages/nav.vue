7cad99989059e0aaaf41cbebc200a9b69961083e
InkCherryA <anniehe1126@gmail.com>
2023-07-29 23:09:29 -0400

    menu 2

--- pages/nav.vue
+++ pages/nav.vue
@@ -4,14 +4,19 @@ export default {
   setup() {
     const nav = useFetch('http://44.207.42.197/wp-json/wp/v2/navigation').data
     const menu = useFetch('http://44.207.42.197/wp-json/wp/v2/menu-items').data
+    const menu2 = useFetch('http://localhost/wp-json/wp/v2/menu-items').data
+
 
     return {
       nav,
       menu,
+      menu2,
     }
   },
   mounted() {
     console.log(this.menu);
+    console.log(this.menu2);
+
   }
 }
 </script>
